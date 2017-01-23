5018a5da801e7ad03bd7ed13a3bacc8732b192ba
fedkul <lukedfischer@yahoo.com>
2017-01-23 15:24:28 -0600

    writing globalSwarm script for swarm creation.  using school of fish code as base.

---
+++ Assets/Scripts/globalSwarm.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalSwarm : MonoBehaviour {
+
+    public GameObject dronePrefab;
+    public static int arenaSize = 5;
+
+    static int numDrone = 10;
+    public static GameObject[] allDrone = new GameObject[numDrone];
+    public static Vector2 goalPos = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < numDrone; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-arenaSize, arenaSize),Random.Range(-arenaSize, arenaSize));
+            allDrone[i] = (GameObject)Instantiate(dronePrefab, pos, Quaternion.identity);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Random.Range(0, 10000) < 50)
+        {
+            goalPos = new Vector2(Random.Range(-arenaSize, arenaSize),
+                                  Random.Range(-arenaSize, arenaSize));
+        }
+    }
+}
